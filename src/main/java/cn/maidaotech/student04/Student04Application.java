cf38198f432cc405e39b4a4db2f92b529d778aa7
like-mac <kelee1987@hotmail.com>
2020-06-28 17:50:54 +0800

    init

---
+++ src/main/java/cn/maidaotech/student04/Student04Application.java
@@ -0,0 +1,13 @@
+package cn.maidaotech.student04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Student04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Student04Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/maidaotech/student04/Student04ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.maidaotech.student04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Student04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
